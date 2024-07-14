0e284a9b5d0f609184f2ec6d8dd0c0118329ee7a
jeiling <a0983681642@gmail.com>
2024-07-14 09:12:07 +0800

    feat: footer ui enhancement

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,4 +1,10 @@
-import { Card, CardBody, CardFooter, Button } from "@nextui-org/react";
+import {
+  Card,
+  Accordion,
+  CardFooter,
+  Button,
+  AccordionItem,
+} from "@nextui-org/react";
 import { Controller, useForm } from "react-hook-form";
 import SelectToken from "./SelectToken";
 import BalanceInput from "./BalanceInput";
@@ -10,15 +16,35 @@ const Form = () => {
     criteriaMode: "all",
   });
 
+  const { from, to } = watch();
+
   const onSubmit = () => {};
+
+  const handleExchange = () => {
+    setValue("from", to);
+    setValue("to", from);
+  };
+
   return (
     <form className="w-full max-w-xs" onSubmit={handleSubmit(onSubmit)}>
       <Card className="flex flex-col gap-y-4 p-4">
-        <SelectToken control={control} />
+        <SelectToken control={control} onExchange={handleExchange} />
         <BalanceInput control={control} />
         <CostInput control={control} />
-        <CardFooter className="flex justify-center">
-          <Button>connect wallet</Button>
+        <CardFooter className="flex flex-col border-t-2 border-indigo-500">
+          <Accordion className="text-sm">
+            <AccordionItem
+              title={
+                <div className="flex justify-between">
+                  You will receive<div>--</div>
+                </div>
+              }
+              className="text-sm"
+            ></AccordionItem>
+          </Accordion>
+          <Button type="submit" color="primary">
+            connect wallet
+          </Button>
         </CardFooter>
       </Card>
     </form>
