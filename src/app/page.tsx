5dac651e9aae2a4561333cba844c5bd753174596
jeiling <a0983681642@gmail.com>
2024-07-15 15:34:56 +0800

    feat: add title to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Form from "./components/Form";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Cross-Chain USDC Transfer</h1>
       <Form />
     </main>
   );
